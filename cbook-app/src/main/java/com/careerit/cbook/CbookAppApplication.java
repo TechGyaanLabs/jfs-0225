15f023e82f0094d5bff3abca6afe9e37c6c67cca
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-05-15 08:28:03 +0530

    cbook-001 refactored code, move download to separate method

--- cbook-app/src/main/java/com/careerit/cbook/CbookAppApplication.java
+++ cbook-app/src/main/java/com/careerit/cbook/CbookAppApplication.java
@@ -1,13 +1,18 @@
 package com.careerit.cbook;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CbookAppApplication {
+public class CbookAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CbookAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Application started successfully");
+	}
 }
