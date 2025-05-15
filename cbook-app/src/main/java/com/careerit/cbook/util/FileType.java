8ef0e389b9b5eba29bdeaa9d9375fa63a74d8452
TechGyaanLabs <techgyaanlabs@gmail.com>
2025-05-15 08:18:47 +0530

    cbook-001 added moved code to enum.

--- cbook-app/src/main/java/com/careerit/cbook/util/FileType.java
+++ cbook-app/src/main/java/com/careerit/cbook/util/FileType.java
@@ -1,5 +1,17 @@
 package com.careerit.cbook.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum FileType {
-    PDF, EXCEL, CSV, JSON
+    PDF, EXCEL, CSV, JSON;
+
+
+    public static List<String> getFileTypes() {
+       List<String> fileTypes = new ArrayList<>();
+         for (FileType fileType : FileType.values()) {
+              fileTypes.add(fileType.name());
+         }
+         return fileTypes;
+    }
 }
